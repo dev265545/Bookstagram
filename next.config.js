fdc4431b18f517c4663e029fa00928a63d327ed1
Dev Gupta <guptadev265@gmail.com>
2022-07-14 01:18:58 +0530

    Inital Commit

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: false,
+};
+
+module.exports = nextConfig;
+module.exports = {
+  images: {
+    domains: ["rb.gy"],
+  },
+};
